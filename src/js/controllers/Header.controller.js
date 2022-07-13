9148b559a1f5bb3ef5d613a10164d8402ac376ad
Talles henrique <tallesco201@gmail.com>
2022-07-13 10:35:52 -0300

    feat : create close button of editprofile
     create close button of edit profile,  style edit title

--- src/js/controllers/Header.controller.js
+++ src/js/controllers/Header.controller.js
@@ -1,70 +1,72 @@
 export default class Header {
-    static buttonCard() {
-        const button = document.getElementById("header--button")
-        const popUp = document.getElementById("wrapper__header--pop-up")
-        const seta = document.getElementById("seta")
+  static buttonCard() {
+    const button = document.getElementById("header--button");
+    const popUp = document.getElementById("wrapper__header--pop-up");
+    const seta = document.getElementById("seta");
 
-        button.addEventListener("click", () => {
+    button.addEventListener("click", () => {
+      if (popUp.style.display === "none") {
+        popUp.style.display = "block";
+      } else if (popUp.style.display === "block") {
+        popUp.style.display = "none";
+      }
 
-            if(popUp.style.display === 'none') {
-                popUp.style.display = 'block'
+      if (seta.style.display === "none") {
+        seta.style.display = "block";
+      } else if (seta.style.display === "block") {
+        seta.style.display = "none";
+      }
+    });
+  }
 
-            }else if(popUp.style.display === 'block') {
-                popUp.style.display = 'none'
+  static buttonOptions() {
+    const buttonEdit = document.getElementById("edit-perfil");
+    const buttonLogout = document.getElementById("logout");
 
-            }
-            
-            if(seta.style.display === 'none') {
-                seta.style.display = 'block'
-                
+    buttonEdit.addEventListener("click", () => {
+      console.log("esse é o botão de editar");
+    });
 
-            }else if(seta.style.display === 'block') {
-                seta.style.display = 'none'
+    buttonLogout.addEventListener("click", () => {
+      console.log("esse é o botão de sair");
+    });
+  }
 
-            }
-        })
-    }
+  static profileHeader(userName, userImg, userDscr) {
+    const wrapperHeader = document.querySelector(".header--img");
+    const wrapperImg = document.querySelector(".wrapper__sub-header--img");
+    const wrapperName = document.querySelector(
+      ".wrapper__sub-header--username"
+    );
 
-    static buttonOptions() {
-        const buttonEdit = document.getElementById("edit-perfil")
-        const buttonLogout = document.getElementById("logout")
-    
-        buttonEdit.addEventListener("click", () => {
-            console.log("esse é o botão de editar")
-        })
+    const imgProfile = document.createElement("img");
+    imgProfile.src = userImg;
+    imgProfile.alt = "user-img";
 
-        buttonLogout.addEventListener("click", () => {
-            console.log("esse é o botão de sair")
-        })
-    }
+    const img = document.createElement("img");
+    img.src = userImg;
+    img.alt = "user-img";
 
-    static profileHeader(userName, userImg, userDscr) {
-        const wrapperHeader = document.querySelector(".header--img")
-        const wrapperImg = document.querySelector(".wrapper__sub-header--img")
-        const wrapperName = document.querySelector(".wrapper__sub-header--username")
-        
-        const imgProfile = document.createElement("img")
-        imgProfile.src = userImg
-        imgProfile.alt = "user-img"
+    const name = document.createElement("p");
+    const dscrp = document.createElement("p");
 
-        const img = document.createElement("img")
-        img.src = userImg
-        img.alt = "user-img"
-        
-        const name = document.createElement("p")
-        const dscrp = document.createElement("p")
+    name.innerText = userName;
+    name.classList.add("user-name");
 
-        name.innerText = userName
-        name.classList.add("user-name")
+    dscrp.innerText = userDscr;
+    dscrp.classList.add("user-description");
 
-        dscrp.innerText = userDscr
-        dscrp.classList.add("user-description")
+    wrapperHeader.appendChild(imgProfile);
+    wrapperImg.appendChild(img);
+    wrapperName.append(name, dscrp);
+  }
 
-        wrapperHeader.appendChild(imgProfile)
-        wrapperImg.appendChild(img)
-        wrapperName.append(name, dscrp)
+  static closeBtnEditProfile() {
+    let container = document.querySelector("#containerProfileModal");
+    let btnClose = document.querySelector("#closeButtonEditProfile");
 
-    }
+    btnClose.addEventListener("click", (event) => {
+      container.classList.add("hidden");
+    });
+  }
 }
-
-
