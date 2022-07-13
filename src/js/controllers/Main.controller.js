41fc2ee627f2d8660fc38470147d083fdc9fa422
yancarlodev <yancarlodc@gmail.com>
2022-07-13 08:38:16 -0300

    fix: fix button Criar to get correct modal

--- src/js/controllers/Main.controller.js
+++ src/js/controllers/Main.controller.js
@@ -1,7 +1,7 @@
 export default class Main {
     static clickButttonCreate () {
         const button = document.querySelector(".section__filter--button")
-        const sectionModal = document.querySelector(".blocker")
+        const sectionModal = document.querySelector("#modal-criar")
         const spanX = document.querySelector(".fa-x")
 
         button.addEventListener("click", event => {
