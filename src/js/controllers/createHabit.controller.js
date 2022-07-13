19864a725309f15ab119ab84f23f2d945e1b6d0e
enilson <enilsonsilva999@gmail.com>
2022-07-13 13:19:49 -0300

    feaut:Api-create-habit

--- src/js/controllers/createHabit.controller.js
+++ src/js/controllers/createHabit.controller.js
@@ -1,19 +1,25 @@
-import LoginRequest from "../models/Api.model.js"
+import Api from "../models/Api.model.js";
 
 export default class createHabit {
-    static controllerHabit(){
-        const buttonInserir = document.querySelector(".button-inserir")
-        buttonInserir.addEventListener("click", async (e) => {
-            e.preventDefault();
-            const dados = {};
-            const formValue = [...e.target.form];
-            formValue.forEach((input) => {
-              if (input.value !== "") {
-                dados[input.name] = input.value;
-              }
-            });
-            console.log(dados)
-        })
-
-    }
+  static controllerHabit() {
+    const modal = document.querySelector("#modal-criar");
+    const pop_up = document.querySelector(".container_pop-up_create-habit");
+    const buttonInserir = document.querySelector(".button-inserir");
+    buttonInserir.addEventListener("click", async (e) => {
+      e.preventDefault();
+      const dados = {};
+      const formValue = [...e.target.form];
+      formValue.forEach((input) => {
+        if (input.value !== "") {
+          dados[input.name] = input.value;
+        }
+      });
+      const request = await Api.createHabit(dados);
+      /* console.log(request);
+      if (request.habit_id !== undefined) {
+        modal.style.display = "none";
+        pop_up.style.display = "flex";
+      } */
+    });
+  }
 }
--- src/js/models/Api.model.js
+++ src/js/models/Api.model.js
@@ -2,6 +2,7 @@ export default class Api {
   static url = "https://habits-kenzie.herokuapp.com/api/userLogin";
   static url_edit = "https://habits-kenzie.herokuapp.com/api/habits/";
   static token = JSON.parse(localStorage.getItem("@kenzie-habit:token"));
+  static user = JSON.parse(localStorage.getItem("@kenzie-habit:user"))
   static headers = {
     "Content-Type": "application/json",
     Authorization: `Bearer ${this.token}`,
@@ -26,7 +27,7 @@ export default class Api {
       .catch((err) => console.log(err));
   }
   static async createHabit(data) {
-    return await fetch(this.url, {
+    return await fetch("https://habits-kenzie.herokuapp.com/api/habits", {
       method: "POST",
       headers: this.headers,
       body: JSON.stringify(data),
