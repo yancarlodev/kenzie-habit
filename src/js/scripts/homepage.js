0f6460282cc4e044f35fb7f77389039cf00e17af
Talles henrique <tallesco201@gmail.com>
2022-07-14 12:56:06 -0300

    fix: Fixed profile picture bug when logging in to save to local storage

--- src/js/scripts/homepage.js
+++ src/js/scripts/homepage.js
@@ -1,26 +1,23 @@
 import Header from "../controllers/Header.controller.js";
 import Main from "../controllers/Main.controller.js";
 import Delete from "../controllers/deleteHabit.controller.js";
-import createHabit from "../controllers/createHabit.controller.js"
-import MainView from "../views/Main.view.js"
+import createHabit from "../controllers/createHabit.controller.js";
+import MainView from "../views/Main.view.js";
 import Api from "../models/Api.model.js";
 
 Header.changeUserImgAndName(Api.user);
-Header.buttonCard()
-Header.editProfile()
-Header.logout()
+Header.buttonCard();
+Header.editProfile();
+Header.logout();
 Header.getInputsEditProfile();
 
+Main.clickButttonCreate();
+Main.showAllComplets();
+Main.showAll();
+createHabit.controllerHabit();
+Delete.deleleHabit();
 
-Main.clickButttonCreate()
-Main.showAllComplets()
-Main.showAll()
-createHabit.controllerHabit()
-Delete.deleleHabit()
-
-MainView.renderAllHabits(Api.habitReadAll())
-Main.clickCheckbox()
-Main.clickButtonEdit()
-MainView.checkIfItsComplete()
-
-
+MainView.renderAllHabits(Api.habitReadAll());
+Main.clickCheckbox();
+Main.clickButtonEdit();
+MainView.checkIfItsComplete();
