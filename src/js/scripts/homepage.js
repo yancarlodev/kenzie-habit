9d8f0bb0558e98bccc9d8220175a62dd7dfa6ba7
Nicole Antunes <102484958+nicoleantuness@users.noreply.github.com>
2022-07-14 00:11:53 -0300

    fix: arrow

--- src/js/scripts/homepage.js
+++ src/js/scripts/homepage.js
@@ -1,15 +1,15 @@
 import Header from "../controllers/Header.controller.js";
 import Main from "../controllers/Main.controller.js";
-
 import createHabit from "../controllers/createHabit.controller.js"
 import MainView from "../views/Main.view.js"
 import Api from "../models/Api.model.js";
 
+Header.changeUserImgAndName(Api.user);
 Header.buttonCard()
 Header.editProfile()
 Header.logout()
-
 Header.getInputsEditProfile();
+
 
 Main.clickButttonCreate()
 createHabit.controllerHabit()
@@ -18,3 +18,5 @@ MainView.renderAllHabits(Api.habitReadAll())
 Main.clickCheckbox()
 Main.clickButtonEdit()
 MainView.checkIfItsComplete()
+
+
